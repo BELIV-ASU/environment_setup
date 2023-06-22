9cd04ffbb3c4379133771450691228a2206e03dd
nehaltkr123 <32680516+nehaltkr123@users.noreply.github.com>
2023-06-21 17:34:03 -0700

    Update install_ros_carla_ue4.sh

--- install_ros_carla_ue4.sh
+++ install_ros_carla_ue4.sh
@@ -1,19 +1,20 @@
 #! bin/bash
 
-sudo apt update && sudo apt upgrade
-sudo apt-get install aria2
-sudo apt install ninja-build
-sudo apt install clang-8
-sudo apt install python-is-python3
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install aria2 -y
+sudo apt install ninja-build -y
+sudo apt install clang-8 -y
+sudo apt install python-is-python3 -y
+sudo apt install pip -y
 
 # ROS2 Galactic Installation
-sudo apt install software-properties-common
-sudo add-apt-repository universe
-sudo apt update && sudo apt install curl gnupg lsb-release
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo apt install software-properties-common -y 
+sudo add-apt-repository universe -y 
+sudo apt update && sudo apt install curl gnupg lsb-release -y 
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg -y
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update # update your apt repo caches
-sudo apt install ros-galactic-desktop
+sudo apt update # update your apt repo caches -y 
+sudo apt install ros-galactic-desktop -y 
 source /opt/ros/galactic/setup.bash
 echo "source /opt/ros/galactic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
@@ -31,10 +32,6 @@ echo "export UE4_ROOT=~/UnrealEngine-4.26" >> ~/.bashrc
 
 # Install and Build CARLA
 
-sudo apt-get install aria2
-sudo apt install ninja-build
-sudo apt install clang-8
-sudo apt install python-is-python3
 pip install --user pygame numpy && pip3 install --user pygame numpy
 pip install --user setuptools && pip3 install --user -Iv setuptools==67.8.0 && pip install --user distro && pip3 install --user distro && pip install --user wheel && pip3 install --user wheel auditwheel
 cd ~/
